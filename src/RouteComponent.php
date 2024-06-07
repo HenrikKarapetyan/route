fb2d72575e6b633ee69f2460755e2f4d59e675af
Henrik Karapetyan <g.karapetyan@tool-kit.tech>
2024-06-07 16:41:51 +0400

    moved subscriber from route to web component

--- src/RouteComponent.php
+++ src/RouteComponent.php
@@ -6,10 +6,6 @@ namespace Henrik\Route;
 
 use Henrik\Contracts\BaseComponent;
 use Henrik\Contracts\ComponentInterfaces\AttributesAndParsersAwareInterface;
-use Henrik\Contracts\ComponentInterfaces\EventSubscriberAwareInterface;
-use Henrik\Contracts\ComponentInterfaces\OnBootstrapAwareInterface;
-use Henrik\Contracts\CoreEvents;
-use Henrik\Contracts\Http\RequestInterface;
 use Henrik\Route\Attributes\Delete;
 use Henrik\Route\Attributes\Get;
 use Henrik\Route\Attributes\Head;
@@ -17,9 +13,8 @@ use Henrik\Route\Attributes\Patch;
 use Henrik\Route\Attributes\Post;
 use Henrik\Route\Attributes\Put;
 use Henrik\Route\Attributes\Route;
-use Henrik\Route\Subscribers\RequestHandlerSubscriber;
 
-class RouteComponent extends BaseComponent implements AttributesAndParsersAwareInterface, EventSubscriberAwareInterface, OnBootstrapAwareInterface
+class RouteComponent extends BaseComponent implements AttributesAndParsersAwareInterface
 {
     /**
      * {@inheritDoc}
@@ -32,32 +27,13 @@ class RouteComponent extends BaseComponent implements AttributesAndParsersAwareI
     public function getAttributesAndParsers(): array
     {
         return [
-            Route::class  => RouteAttributesParser::class,
-            Get::class    => RouteAttributesParser::class,
-            Post::class   => RouteAttributesParser::class,
-            Put::class    => RouteAttributesParser::class,
-            Patch::class  => RouteAttributesParser::class,
-            Head::class   => RouteAttributesParser::class,
+            Route::class => RouteAttributesParser::class,
+            Get::class => RouteAttributesParser::class,
+            Post::class => RouteAttributesParser::class,
+            Put::class => RouteAttributesParser::class,
+            Patch::class => RouteAttributesParser::class,
+            Head::class => RouteAttributesParser::class,
             Delete::class => RouteAttributesParser::class,
-        ];
-    }
-
-    public function getEventSubscribers(): array
-    {
-        return [
-            CoreEvents::ROUTE_DISPATCHER_DEFAULT_DEFINITION_ID => [RequestHandlerSubscriber::class],
-        ];
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function onBootstrapDispatchEvents(): array
-    {
-        return [
-            CoreEvents::ROUTE_DISPATCHER_DEFAULT_DEFINITION_ID => [
-                RequestInterface::class => CoreEvents::HTTP_REQUEST_EVENTS,
-            ],
         ];
     }
 }
